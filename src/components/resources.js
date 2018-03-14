dbd8a8f508626e513249ff838e31cc3fb16b0727
Dom Bellettini <dombellettini@gmail.com>
2018-03-13 18:52:31 -0700

    2nd commit - final functionality of HOC

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			Super Special Secret Recipe
+			<ul>
+				<li>1 Cup of Sugar</li>
+				<li>1 Cup of Salt</li>
+				<li>4 Tablespoons of Butter</li>
+			</ul>
+		</div>
+	);
+}
